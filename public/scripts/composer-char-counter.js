645fe32695d3e37d30a98c2ff53bba7881f35cee
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-08 23:43:42 +0000

    Now the nav bar is fixed and does not affect the other objects' display. Eslinted composer-char-counter.js

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,16 @@
 
 // make sure that the document is ready before we run our code
-$(document).ready(function () {
+$(document).ready(function() {
 
   // add an event listen to handle actions after the user stop typing. It uses .bind() instead of on() to leverage THIS
-  $("#tweet-text").bind("keyup", function (event) {
+  $("#tweet-text").bind("keyup", function() {
     // assign the text length to a variable
     const value = 140 - $(this).val().length;
     // use the text length to update the counter that is in the same form as the textarea
-    $(this).parent().find(".counter").text(value)
+    $(this).parent().find(".counter").text(value);
     // check whether the counter is negative and assign a class to change its style
-    value < 0 ? $(".counter").addClass("negative-value") : $(".counter").removeClass("negative-value")
+    value < 0 ? $(".counter").addClass("negative-value") : $(".counter").removeClass("negative-value");
  
-  })
+  });
 
 });
