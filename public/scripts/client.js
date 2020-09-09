f305c74c8fd2443aee82fd81f677e71c4d1632f0
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-09 16:59:59 +0000

    Add the createTweetElement function that receives an tweet object and returns the html markup to be rendered in the #tweets-container element. Found a bug that whenever a tweet is not being hovered, it stay on top of our nav bar

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,3 +4,79 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const arr = [
+  {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  },
+  {
+    "user": {
+      "name": "Descartes",
+      "avatars": "https://i.imgur.com/nlhLi3I.png",
+      "handle": "@rd"
+    },
+    "content": {
+      "text": "Je pense , donc je suis"
+    },
+    "created_at": 1461113959088
+  }
+];
+
+
+$(document).ready(() => {
+
+  const createTweetElement = (tweetObj) => {
+    const tweet = $(`
+      <article class="tweet">
+        <header>
+          <div>
+            <img src="${tweetObj.user.avatars}">
+            <p>${tweetObj.user.name}</p>
+          </div>
+          <a href="#">${tweetObj.user.handle}</a>
+        </header>
+        <p>${tweetObj.content.text}</p>
+        <footer>
+          <p>10 days ago</p>
+          <div>
+            <span>&#9873</span>
+            <span>&#8633</span>
+            <span>&#9829</span>
+          </div>
+        </footer>
+      </article>
+    `)
+    return tweet;
+  };
+
+  // Test / driver code (temporary). Eventually will get this from the server.
+  const tweetData = {
+    "user": {
+      "name": "Newton",
+      "avatars": "https://i.imgur.com/73hZDYK.png",
+      "handle": "@SirIsaac"
+    },
+    "content": {
+      "text": "If I have seen further it is by standing on the shoulders of giants"
+    },
+    "created_at": 1461116232227
+  }
+
+
+  const $tweet = createTweetElement(tweetData);
+
+  // Test / driver code (temporary)
+  console.log($tweet); // to see what it looks like
+  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
+
+
+
+
+});
