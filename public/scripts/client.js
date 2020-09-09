401a12eeb10e140ae205477d3231054fdbdb40d2
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-09 17:44:06 +0000

    Built the renderTweets function that given an array of tweet objects, it renders each tweet object in #tweets-container element

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,36 +4,11 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const arr = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense , donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
-
-
 $(document).ready(() => {
 
   const createTweetElement = (tweetObj) => {
-    const tweet = $(`
+    // still need some work so that the date is calculated instead of giving a fixed value of 10...
+    const $tweet = $(`
       <article class="tweet">
         <header>
           <div>
@@ -44,7 +19,7 @@ $(document).ready(() => {
         </header>
         <p>${tweetObj.content.text}</p>
         <footer>
-          <p>10 days ago</p>
+          <p>${10} days ago</p>
           <div>
             <span>&#9873</span>
             <span>&#8633</span>
@@ -53,30 +28,43 @@ $(document).ready(() => {
         </footer>
       </article>
     `)
-    return tweet;
+    return $tweet;
+  };
+
+  const renderTweets = tweetsArray => {
+    tweetsArray.forEach(elem => {
+      const $tweet = createTweetElement(elem);
+      $('#tweets-container').append($tweet);
+    })
+
   };
 
   // Test / driver code (temporary). Eventually will get this from the server.
-  const tweetData = {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
+  const data = [
+    {
+      "user": {
+        "name": "Newton",
+        "avatars": "https://i.imgur.com/73hZDYK.png",
+        "handle": "@SirIsaac"
+      },
+      "content": {
+        "text": "If I have seen further it is by standing on the shoulders of giants"
+      },
+      "created_at": 1461116232227
     },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  }
-
-
-  const $tweet = createTweetElement(tweetData);
-
-  // Test / driver code (temporary)
-  console.log($tweet); // to see what it looks like
-  $('#tweets-container').append($tweet); // to add it to the page so we can make sure it's got all the right elements, classes, etc.
-
-
+    {
+      "user": {
+        "name": "Descartes",
+        "avatars": "https://i.imgur.com/nlhLi3I.png",
+        "handle": "@rd"
+      },
+      "content": {
+        "text": "Je pense , donc je suis"
+      },
+      "created_at": 1461113959088
+    }
+  ];
+  renderTweets(data);
 
 
 });
