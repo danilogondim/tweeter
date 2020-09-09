bb087003e30b3959d203b54c14613bf6daea6250
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-09 20:10:19 +0000

    Add a loadTweets function that makes an AJAX get request so we can render the tweets array in our app

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -40,42 +40,27 @@ $(document).ready(() => {
 
   };
 
-  // Test / driver code (temporary). Eventually will get this from the server.
-  const data = [
-    {
-      "user": {
-        "name": "Newton",
-        "avatars": "https://i.imgur.com/73hZDYK.png",
-        "handle": "@SirIsaac"
-      },
-      "content": {
-        "text": "If I have seen further it is by standing on the shoulders of giants"
-      },
-      "created_at": 1461116232227
-    },
-    {
-      "user": {
-        "name": "Descartes",
-        "avatars": "https://i.imgur.com/nlhLi3I.png",
-        "handle": "@rd"
-      },
-      "content": {
-        "text": "Je pense , donc je suis"
-      },
-      "created_at": 1461113959088
-    }
-  ];
-  renderTweets(data);
-
-
   $('form').on('submit', function (evt) {
     evt.preventDefault();
     $.ajax({ url: '/tweets/', data: $(this).serialize(), method: "POST" }).then(response => {
+      // maybe remove the then method in the final version?
       console.log("Success!")
     }).catch(e => {
+      // maybe remove the catch method in the final version?
       console.log("Failed!");
     })
   })
+
+  const loadTweets = () => {
+    $.ajax({ url: '/tweets/', method: "GET" }).then(response => {
+      console.log("Successful load!!");
+      renderTweets(response);
+    }).catch(e => {
+      // maybe remove the catch method in the final version?
+      console.log("Failed to load tweets");
+    })
+  }
+  loadTweets();
 
 
 });
