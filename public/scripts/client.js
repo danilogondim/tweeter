13f25ddeac7846a14c07c31c4d2af67acd38595f
Danilo Gondim <daniloencarnacao@gmail.com>
2020-09-10 00:35:13 +0000

    Change the anchor element to a proper button element on the nav bar

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -7,14 +7,15 @@
 $(document).ready(() => {
 
   // add a event listener to the nav anchor element to toggle the submission form
-  $("nav > a").on('click', (evt) => {
-    evt.preventDefault();
+  $("#compose-button").on('click', (evt) => {
+    // evt.preventDefault();
     // if the form is not visible, slide down the form
     if ($('.new-tweet:visible').length === 0) {
       $('.new-tweet').slideDown();
       return $("#tweet-text").focus();
     }
-    // else, hide the form
+    // else, unfocus the button and hide the form
+    $("#compose-button").blur()
     return $('.new-tweet').slideUp();
   })
 
